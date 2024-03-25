6f3f77139a04e02b19fe6f1d6d6d22ac44265f0f
Khushi Singh <khushisingh3sep2004@gmail.com>
2024-03-25 23:06:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "7c622ad54306464589d607661739f843";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
